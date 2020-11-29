c44353010d6e9ea90fb63e613f47b70dd15bbda2
ShykhDanila <pizuk5839@gmail.com>
2020-11-29 21:45:13 +0200

    create repository

---
+++ SocialNetwork/src/main/java/shykh/project/SocialNetwork/repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package shykh.project.SocialNetwork.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import shykh.project.SocialNetwork.model.Message;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, String> {
+}
