ce42af9a092396fd3bab77d6ec206d59e2092814
ShykhDanila <pizuk5839@gmail.com>
2020-12-06 00:10:55 +0200

    create message controller

--- SocialNetwork/src/main/java/shykh/project/SocialNetwork/controller/MessageController.java
+++ SocialNetwork/src/main/java/shykh/project/SocialNetwork/controller/MessageController.java
@@ -6,12 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shykh.project.SocialNetwork.model.Message;
+import shykh.project.SocialNetwork.model.User;
 import shykh.project.SocialNetwork.service.impl.MessageServiceImpl;
 import shykh.project.SocialNetwork.service.impl.UserServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Objects;
 
 @Slf4j
 @RestController
@@ -19,35 +19,41 @@ public class MessageController {
 
     @Autowired
     MessageServiceImpl service;
+
+    @Autowired
     UserServiceImpl userService;
 
     @PostMapping("/message")
-    public ResponseEntity<?> createMessage(@RequestBody Message message, HttpServletRequest req){
+    public ResponseEntity<Message> createMessage(HttpServletRequest req){
+        User user = userService.getByEmail(req.getSession().getAttribute("email").toString());
+
+        Message message = new Message(user.getId(), req.getSession().getAttribute("idOtherUser").toString(), req.getParameter("text"));
+
+        System.out.println("message : " + message);
+
+        message.addUser(user); // Падає
         service.create(message);
-        message.addUser(userService.getByEmail(req.getSession().getAttribute("email").toString()));
+
+        user.addMessage(message);
+
         log.info("Message {} was added", message);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
     }
 
-    @GetMapping("/message")
-    public ResponseEntity<List<Message>> getAllMessages(){
-        log.info("Looking for all messages");
-        List<Message> messages = service.getAllMessages();
-        if (messages.isEmpty()){
-            log.info("No records found");
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @PostMapping("/messages")
+    public ResponseEntity<List<Message>> showAllMessages( HttpServletRequest req){
+        User iUser = userService.getByEmail(req.getSession().getAttribute("email").toString());
+        String idOtherUser = req.getParameter("idOtherUser");
+        System.out.println("id : " + idOtherUser);
+        User otherUser = userService.getById(idOtherUser);
+        log.info("Looking for history messages");
+        List<Message> messages = null;
+        if (iUser.getMessages().size() != 0){
+            messages = service.getMyMessagesAndResponse(iUser.getId(), otherUser.getId());
+            log.info("Message history was load");
         }
+        //MessageResponse messageResponses = new MessageResponse(messages, otherUser);
+        req.getSession().setAttribute("idOtherUser",otherUser.getId());
         return new ResponseEntity<>(messages, HttpStatus.OK);
-    }
-
-    @GetMapping("/message/{id}")
-    public ResponseEntity<Message> getMessageById(@PathVariable("id") String id){
-        log.info("Looking for a message by id {}", id);
-        Message message = service.getById(id);
-        if (Objects.isNull(message)){
-            log.error("No message by id {}", id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
