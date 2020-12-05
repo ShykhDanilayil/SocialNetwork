c4f48ff59d357070d78a7385c1a64de08961d7fb
ShykhDanila <pizuk5839@gmail.com>
2020-12-06 00:11:31 +0200

    create message repository

--- SocialNetwork/src/main/java/shykh/project/SocialNetwork/repository/MessageRepository.java
+++ SocialNetwork/src/main/java/shykh/project/SocialNetwork/repository/MessageRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import shykh.project.SocialNetwork.model.Message;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, String> {
 
-
+    List<Message> findAllByIdUserFromAndIdUserToOrIdUserFromAndIdUserTo(String idUserFrom, String idUserTo, String idUserFrom2, String idUserTo2);
 }
