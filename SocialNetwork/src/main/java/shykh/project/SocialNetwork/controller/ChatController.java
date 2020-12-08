0f61c020a470b591df11b5974f53d650775acfce
ShykhDanila <pizuk5839@gmail.com>
2020-12-08 17:52:08 +0200

    controller

--- SocialNetwork/src/main/java/shykh/project/SocialNetwork/controller/ChatController.java
+++ SocialNetwork/src/main/java/shykh/project/SocialNetwork/controller/ChatController.java
@@ -6,12 +6,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shykh.project.SocialNetwork.model.Message;
 import shykh.project.SocialNetwork.model.User;
 import shykh.project.SocialNetwork.model.response.UserAndLastMessage;
+import shykh.project.SocialNetwork.service.impl.MessageServiceImpl;
 import shykh.project.SocialNetwork.service.impl.UserServiceImpl;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @RestController
@@ -20,18 +24,29 @@ public class ChatController {
     @Autowired
     UserServiceImpl service;
 
+    @Autowired
+    MessageServiceImpl messageService;
+
     @GetMapping("/chatPeople")
-    public ResponseEntity<List<UserAndLastMessage>> getUsersInChat() {
+    public ResponseEntity<List<UserAndLastMessage>> getUsersInChat(HttpServletRequest req) {
+        User iUser = service.getByEmail(req.getSession().getAttribute("email").toString());
         log.info("Looking for all users");
         List<User> users = service.getAllUsers();
+        users.remove(iUser);
         if (users.isEmpty()){
             log.info("No records found");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        List<Message> messages = null;
         List<UserAndLastMessage> usersChat = new ArrayList<>();
         for (User user:
                 users) {
-            usersChat.add(new UserAndLastMessage(user));
+            UserAndLastMessage userAndLastMessage = new UserAndLastMessage(user);
+            if (messageService.getMyMessagesAndResponse(iUser.getId(), user.getId()).size() != 0){
+                messages = messageService.getMyMessagesAndResponse(iUser.getId(), user.getId());
+            userAndLastMessage.addLastMess(messages.get(messages.size() - 1));
+        }
+            usersChat.add(userAndLastMessage);
         }
         return new ResponseEntity<>(usersChat, HttpStatus.OK);
     }
