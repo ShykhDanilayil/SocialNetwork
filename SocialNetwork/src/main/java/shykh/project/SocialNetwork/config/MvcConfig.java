b868a2a7521048caab42a88590e7bccd91565d0d
ShykhDanila <pizuk5839@gmail.com>
2020-12-15 12:03:37 +0200

    edit confiquration

--- SocialNetwork/src/main/java/shykh/project/SocialNetwork/config/MvcConfig.java
+++ SocialNetwork/src/main/java/shykh/project/SocialNetwork/config/MvcConfig.java
@@ -9,5 +9,8 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/logIn").setViewName("index.html");
         registry.addViewController("/chat").setViewName("chatBox.html");
+        registry.addViewController("/cabinet").setViewName("cabinet.html");
+        registry.addViewController("/profile").setViewName("profile.html");
+        registry.addViewController("/allUsers").setViewName("allUsers.html");
     }
 }
